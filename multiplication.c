877391a8b0984663afac373e6f9c5c6aebfd4899
ahmed yousry <63128998+AhmedYousry49@users.noreply.github.com>
2024-11-14 21:16:11 +0200

    Full version (#8)
    * Add multiplication functions
    
    * Implemented Addition
    
    * Done Subtraction Feature
    
    * add Division Function
    
    * Add multiplication functions
    
    * Add power functions
    
    * change from float to double
    
    * run
    
    ---------
    
    Co-authored-by: mohamedawadin01@gmail.com <mohamedawadin01@gmail.com>
    Co-authored-by: AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
    Co-authored-by: Omar Mohamed <omarabdalqawi@gmail.com>
    Co-authored-by: Esraa Fawzy <103899204+Esraa-f28@users.noreply.github.com>
    Co-authored-by: MohamedAwadin <80068598+MohamedAwadin@users.noreply.github.com>
    Co-authored-by: Abdelrhman Abdelaziz Fayed <92241318+AbdelrhmanAFayed@users.noreply.github.com>
    Co-authored-by: YoussefMostafaMohammed <youssefmostafa453@gmail.com>

---
+++ multiplication.c
@@ -0,0 +1,15 @@
+#include "multiplication.h"
+
+
+int multiply_int(int a, int b) {
+    return a * b;
+}
+
+
+float multiply_float(float a, float b) {
+    return a * b;
+}
+
+double multiply_double(double a, double b) {
+    return a * b;
+}

5969087609ab7ffdc8b81941177a5bde2cfa37db
ahmed yousry <ahmedyousry449@gmail.com>
2024-11-14 21:05:36 +0200

    run

--- main.c
+++ main.c
@@ -1,27 +1,70 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "multiplication.h"
+#include "remainder.h"
+#include "subtraction.h"
+#include "addition.h"
+#include "power.h"
 #include "division.h"
 
 int main()
 {
-    printf("Division Program Test\n");
+    int choice;
+    double num1, num2;
+    int dividend, divisor;
 
-    // Define and run test cases
-    float testCases[][2] = {
-        {10, 2},  // Normal division
-        {7, 0},   // Division by zero
-        {15, -3}, // Division with negative number
-        {-9, 3},  // Division with negative dividend
-        {0, 5},   // Zero divided by a number
-    };
-
-    int numTestCases = sizeof(testCases) / sizeof(testCases[0]);
-
-    for (int i = 0; i < numTestCases; i++)
+    while (1)
     {
-        float result = division(testCases[i][0], testCases[i][1]);
-        printf("Test case %d: %.2f / %.2f = %.2f\n", i + 1, testCases[i][0], testCases[i][1], result);
+
+        printf("Simple Calculator\n");
+        printf("1. Subtraction\n");
+        printf("2. Calculate Remainder\n");
+        printf("3. Multiplication\n");
+        printf("4. Addition\n");
+        printf("5. Power\n");
+        printf("6. Division\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", subtraction(num1, num2));
+            break;
+
+        case 2:
+            printf("Enter dividend and divisor: ");
+            scanf("%d %d", &dividend, &divisor);
+            printf("Remainder: %d\n", calculate_remainder(dividend, divisor));
+            break;
+
+        case 3:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", multiply_double(num1, num2));
+            break;
+
+        case 4:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", addition(num1, num2));
+            break;
+        case 5:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", powerFunction(num1, num2));
+            break;
+        case 6:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", division(num1, num2));
+            break;
+
+        default:
+            printf("Invalid choice\n");
+        }
     }
-  
-  return 0 ;
-  
+
+    return 0;
 }

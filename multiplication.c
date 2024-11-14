8f6651db5418ceafbe03801f3f048219276263d2
mohamedawadin01@gmail.com <mohamedawadin01@gmail.com>
2024-11-14 11:04:43 +0200

    Add multiplication functions

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include "multiplication.h"
+
+
+int multiply_int(int a, int b) {
+    return a * b;
+}
+
+
+float multiply_float(float a, float b) {
+    return a * b;
+}
---
+++ multiplication.h
@@ -0,0 +1,10 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+
+
+int multiply_int(int a, int b);
+
+
+float multiply_float(float a, float b);
+
+#endif // MULTIPLICATION_H

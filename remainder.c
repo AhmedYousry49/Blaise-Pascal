73b6323439b2cba90bc0b9fe7b2dc5f6a81717cd
ahmedyousry49 <ahmedyousry449@gmail.com>
2024-11-14 12:02:40 +0200

    remainder

---
+++ remainder.c
@@ -0,0 +1,11 @@
+// remainderm.c
+#include "remainder.h"
+
+// Function definition to calculate remainder
+int calculate_remainder(int dividend, int divisor)
+{
+
+    if (divisor == 0)//Error: Division by zero is not allowed.\n        
+        return -1; 
+    return dividend % divisor;
+}
---
+++ remainder.h
@@ -0,0 +1,8 @@
+// remainderm.h
+#ifndef REMAINDERM_H
+#define REMAINDERM_H
+
+// Function prototype to calculate remainder
+int calculate_remainder(int dividend, int divisor);
+
+#endif // REMAINDERM_H

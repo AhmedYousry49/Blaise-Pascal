3f214b37b7242dc061dccdcfb02bcdd031dd0a5a
ahmed yousry <63128998+AhmedYousry49@users.noreply.github.com>
2024-11-14 21:16:11 +0200

    Full version (#8)
    * Add multiplication functions
    
    * Implemented Addition
    
    * Done Subtraction Feature
    
    * add Division Function
    
    * Add multiplication functions
    
    * Add power functions
    
    * change from float to double
    
    * run
    
    ---------
    
    Co-authored-by: mohamedawadin01@gmail.com <mohamedawadin01@gmail.com>
    Co-authored-by: AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
    Co-authored-by: Omar Mohamed <omarabdalqawi@gmail.com>
    Co-authored-by: Esraa Fawzy <103899204+Esraa-f28@users.noreply.github.com>
    Co-authored-by: MohamedAwadin <80068598+MohamedAwadin@users.noreply.github.com>
    Co-authored-by: Abdelrhman Abdelaziz Fayed <92241318+AbdelrhmanAFayed@users.noreply.github.com>
    Co-authored-by: YoussefMostafaMohammed <youssefmostafa453@gmail.com>

--- main.c
+++ main.c
@@ -1,15 +1,69 @@
 #include <stdio.h>
+#include "multiplication.h"
 #include "remainder.h"
+#include "subtraction.h"
+#include "addition.h"
+#include "power.h"
+#include "division.h"
 
 int main()
 {
+    int choice;
+    double num1, num2;
+    int dividend, divisor;
 
     while (1)
     {
-        /* code */
-        int y, k;
-        scanf("%d %d", &y, &k);
-        printf("%d", calculate_remainder(y, k));
+
+        printf("Simple Calculator\n");
+        printf("1. Subtraction\n");
+        printf("2. Calculate Remainder\n");
+        printf("3. Multiplication\n");
+        printf("4. Addition\n");
+        printf("5. Power\n");
+        printf("6. Division\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", subtraction(num1, num2));
+            break;
+
+        case 2:
+            printf("Enter dividend and divisor: ");
+            scanf("%d %d", &dividend, &divisor);
+            printf("Remainder: %d\n", calculate_remainder(dividend, divisor));
+            break;
+
+        case 3:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", multiply_double(num1, num2));
+            break;
+
+        case 4:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", addition(num1, num2));
+            break;
+        case 5:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", powerFunction(num1, num2));
+            break;
+        case 6:
+            printf("Enter two numbers: ");
+            scanf("%lf %lf", &num1, &num2);
+            printf("Result: %.2lf\n", division(num1, num2));
+            break;
+
+        default:
+            printf("Invalid choice\n");
+        }
     }
 
     return 0;

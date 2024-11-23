797f425b6c4bb7ebd2d98ba595353c6cb76b27aa
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2024-11-23 10:23:30 +0200

    Enhance Structure

--- build/lib/multiplication/CMakeFiles/multiplication.dir/src/multiplication.c.obj.d
+++
@@ -1,2 +0,0 @@
-lib/multiplication/CMakeFiles/multiplication.dir/src/multiplication.c.obj: \
- D:\work\ITI\Blaise-Pascal\Calculator_Blaise_Pascal\Blaise-Pascal\lib\multiplication\src\multiplication.c
--- build/lib/subtraction/CMakeFiles/subtraction.dir/src/subtraction.c.obj.d
+++
@@ -1,2 +0,0 @@
-lib/subtraction/CMakeFiles/subtraction.dir/src/subtraction.c.obj: \
- D:\work\ITI\Blaise-Pascal\Calculator_Blaise_Pascal\Blaise-Pascal\lib\subtraction\src\subtraction.c
